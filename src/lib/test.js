562adbf2f5dc58a65a547aa4a683ec2f5874760d
kichyr <kichyr@mail.ru>
2018-10-11 23:53:42 +0300

    hw1 v4

--- src/lib/test.js
+++ src/lib/test.js
@@ -1,10 +1,8 @@
 const say = function (name) {
-    var newDiv = document.createElement("div");
-    newDiv.innerHTML = `Hello ${name}`
-    newDiv.setAttribute("class", "box")
-    document.body.appendChild(newDiv);
-    return `Hello ${name}`;
-  }
-  
-  export default say;
-  
+  var newDiv = document.createElement("div");
+  newDiv.innerHTML = `Hello ${name}`
+  newDiv.setAttribute("class", "box")
+  document.body.appendChild(newDiv);
+  return `Hello ${name}`;
+}
+export default say;
