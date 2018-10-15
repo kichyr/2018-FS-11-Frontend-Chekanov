30a50f1bbae4e66ad2b7648306dfc21faa3f5466
kichyr <kichyr@mail.ru>
2018-10-15 22:31:01 +0300

    fixing hw1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-undef */
 import say from './lib/test.js';
 
 alert(say('world'));
+/* eslint-enable no-undef */
--- src/lib/test.js
+++ src/lib/test.js
@@ -1,8 +1,9 @@
+/* eslint-env browser */
 const say = function (name) {
-  var newDiv = document.createElement("div");
-  newDiv.innerHTML = `Hello ${name}`
-  newDiv.setAttribute("class", "box")
+  const newDiv = document.createElement('div');
+  newDiv.innerHTML = `Hello ${name}`;
+  newDiv.setAttribute('class', 'box');
   document.body.appendChild(newDiv);
   return `Hello ${name}`;
-}
+};
 export default say;
